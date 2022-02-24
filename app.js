1351120cbff98e867dc1a89793671b8e7b3defd7
Ykhoo <89803794+YukoHo0513@users.noreply.github.com>
2022-02-23 20:47:54 -0800

    close button added and layout fixed

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ function displayEmployees(employeeData) {
         employeeHTML += `
             <div class="card" data-index="${index}">
                 <img src="${picture}" alt="${name}'s profile photo" class="avatar">
-                <div>
+                <div class="profile">
                     <h2>${name}</h2>
                     <p>${email}</p>
                     <p>${city}</p>
@@ -36,14 +36,14 @@ function displayEmployees(employeeData) {
 }
 
 function displayModal(index) {
+    console.log(index)
     let name = employees[index].name.first + " " + employees[index].name.last;
     let email = employees[index].email;
     let city = employees[index].location.city;
-    let picture = employees[index].picture.medium; 
+    let picture = employees[index].picture.large; 
     let phone = employees[index].phone;
-    let dob = employees[index].dob;
-    let date = new Date(dob);
-    date.toLocaleDateString('en-US');
+    let dob = employees[index].dob.date;
+    let date = (new Date(dob)).toLocaleDateString('en-US');
     let address = employees[index].location.street.number + " " + employees[index].location.street.name + ", " + employees[index].location.state + " " + employees[index].location.postcode
     modal.innerHTML = `
         <img src="${picture}" alt="${name}'s profile photo">
@@ -54,11 +54,32 @@ function displayModal(index) {
             <div class="border"></div>
             <p class="phone">${phone}</p>
             <p>${address}</p>
-            <p>${date}</p>
+            <p>Birthday: ${date}</p>
         </div>
     `;
     overlay.classList.remove("hidden");
 }
+profileContainer.addEventListener('click', (e) => {
+    overlay.className = "overlay"
+    if (e.target.className === "card" || e.target.parentElement.className === "profile" || e.target.parentElement.className === "card") {
+        if (e.target.className === "card") {
+            let stringIndex = e.target.getAttribute("data-index");
+            let numIndex = parseInt(stringIndex);
+            displayModal(numIndex);
+        } else if (e.target.parentElement.className === "profile") {
+            let stringIndex = e.target.parentElement.parentElement.getAttribute("data-index");
+            let numIndex = parseInt(stringIndex);
+            displayModal(numIndex);
+        } else if (e.target.parentElement.className === "card") {
+            let stringIndex = e.target.parentElement.getAttribute("data-index");
+            let numIndex = parseInt(stringIndex);
+            displayModal(numIndex);
+        }
+    }   
+})
+closeButton.addEventListener('click', ()=> {
+    overlay.className = "hidden";
+}) 
 function checkStatus(response) {
     if (response.ok) {
         return Promise.resolve(response);
@@ -66,4 +87,4 @@ function checkStatus(response) {
         return Promise.reject(new Error(response.statusText));
     }
 }
-console.log("words",fetchData(urlAPI));
+fetchData(urlAPI);
